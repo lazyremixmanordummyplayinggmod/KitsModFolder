c608896e09c963f77f999de74b069889ec6b079c
PringleKitten <wie054024@gmail.com>
2025-02-12 23:04:37 -0600

    V3.11.1
    Video Sprite Problem Fix?

--- Kitty/scripts/videoSprite.lua
+++ Kitty/scripts/videoSprite.lua
@@ -8,3 +8,17 @@ function makeVideoSprite(tag, videoPath,camera,zoom)
     setProperty('defaultCamZoom',zoom)
     setProperty("camZooming", amieven)
 end
+
+function onDestroy()
+    setProperty('inCutscene', false);
+    callMethod('remove', {instanceArg('videoCutscene'), true})
+    removeLuaSprite("videoCutscene")
+    setProperty('canPause', true)
+end
+function onPause()
+    callMethod('videoCutscene.pause')
+end
+
+function onResume()
+    callMethod('videoCutscene.resume')
+end
